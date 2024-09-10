d25318349abe786a1ffc41652ec63ee4bdcb75cb
Kamila Aibedullova <kamila.aibedullova@drsmile.com>
2024-09-09 20:41:18 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import MultiplicationMachine from "./multitable/MultiplicationMachine";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Multiplication table from 1 to 9</h1>
-        <h2>Just press the equation buttons</h2>
-        <h2>and answers pop up!</h2>
+        <h1>Just press the equation buttons</h1>
+        <h1>and answers pop up!</h1>
         <div className="parent-container">
           <MultiplicationMachine/>
         </div>
